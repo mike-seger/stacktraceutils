5a9ab77a54d01da30050bb1a452c9ecec9c0df00
michael <michael@n.c>
2018-09-21 01:31:37 +0000

    alternative output directory, updated README

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # stacktraceutils
 
-## Example
+## Examples
 ```
-# parse all exceptions from log files (gz also allowed)
+# parse all exceptions from log files (gz also allowed) and extract an
+# example each in the output directory (out)
 ./parse-exceptions.sh /var/log/tomcat8/catalina* | ./extract-exception-examples.sh -
 ```
--- extract-exception-examples.sh
+++ extract-exception-examples.sh
@@ -9,25 +9,28 @@ tempfile() {
 TMP="$(tempfile)"
 trap 'rm -f $TMP' EXIT
 
+outdir=out
 if [ "$1" == '-' ] ; then
 	cat >"$TMP"
 	exceptions="$TMP"
 elif [ ! -f "$1" ] ; then
-	echo "Usage: $0 <exceptions-file|>"
+	echo "Usage: $0 <exceptions-file|> [outdir]"
 	echo "Use - for stdin"
 	exit 0	
 fi
 
-mkdir -p out
+if [ "$#" -ge 2 ] ; then
+	outdir=$2	
+fi
+
+mkdir -p "$outdir"
 
 cat $exceptions | $(dirname $0)/stats-exceptions.sh | sed -e "s/ /\\t/" | while read line ; do
 	pat=$(echo "$line" | tr "\t" " " | tr -s " " | sed -e "s/ /.*/g")
 	n=$(grep -c "$pat" "$exceptions")
 	m=$(echo "$line" | sed -e "s/\\t.*//")
-#echo $line
-#echo "$pat"
 	file=$(printf "%05d\t%s\n" "$n" "$line"| \
 		cut -b1-60|cut -f1,3|tr "\t" " "|sed -E "s/ *$//;s/ [a-zA-Z]{0,3}$//;").txt
-	grep "$pat" "$exceptions" | sort -u | tail -1 |sed -E "s/\\\\n/\\n/g">out/"$file"
+	grep "$pat" "$exceptions" | sort -u | tail -1 |sed -E "s/\\\\n/\\n/g">"$outdir"/"$file"
 done
-echo "Created exception examples in directory 'out'"
+echo "Created exception examples in directory '$outdir'"
